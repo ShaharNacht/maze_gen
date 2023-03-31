9da7ab2afa2cbbbc2204eb956adbc1d9d7b98120
Shahar <shahar.nacht@gmail.com>
2023-03-31 15:51:53 +0300

    Added maze wall struct (not used yet)

--- src/maze.rs
+++ src/maze.rs
@@ -4,6 +4,7 @@ use rand::thread_rng;
 use rand::rngs::ThreadRng;
 use rand::seq::SliceRandom;
 
+use crate::str_err::Result;
 use crate::point::MazePoint;
 
 pub struct Maze
@@ -11,6 +12,7 @@ pub struct Maze
 	width: i64,
 	height: i64,
 	
+	walls: HashSet<Wall>,
 	path: Vec<MazePoint>,
 	visited: HashSet<MazePoint>,
 	
@@ -23,6 +25,9 @@ impl Maze
 	{
 		let start_point = MazePoint::new( 0, 0 );
 		
+		let mut walls = HashSet::new();
+		Self::fill_all_walls( &mut walls, width, height );
+		
 		let path = vec![start_point];
 		
 		let mut visited = HashSet::new();
@@ -30,7 +35,7 @@ impl Maze
 		
 		let rng = thread_rng();
 		
-		Self { width, height, path, visited, rng }
+		Self { width, height, walls, path, visited, rng }
 	}
 	
 	pub fn width(&self) -> i64
@@ -89,5 +94,82 @@ impl Maze
 			point.x < self.width &&
 			point.y >= 0 &&
 			point.y < self.height
+	}
+	
+	fn fill_all_walls( walls: &mut HashSet<Wall>, width: i64, height: i64 )
+	{
+		for y in 0 .. height - 1
+		{
+			for x in 0 .. width - 1
+			{
+				let current = MazePoint::new( x, y );
+				let right = current + ( 1, 0 );
+				let down = current + ( 0, 1 );
+				
+				walls.insert( Wall::new( current, right ).unwrap() );
+				walls.insert( Wall::new( current, down ).unwrap() );
+			}
+		}
+	}
+}
+
+/// A wall between two cells in a Maze
+/// 
+/// Enforces an invariat to make sure the two cells are adjacent,
+/// and that the internal order of the cells doesn't depend on the provided order,
+/// so that equality checks and hashes don't depend the order.
+#[derive( PartialEq, Eq, Hash )]
+struct Wall( MazePoint, MazePoint );
+
+impl Wall
+{
+	fn new( cell1: MazePoint, cell2: MazePoint ) -> Result<Self>
+	{
+		let x_diff = ( cell2.x - cell1.x ).abs();
+		let y_diff = ( cell2.y - cell1.y ).abs();
+		
+		if x_diff + y_diff != 1
+		{
+			return Err( "A wall must be between two adjacent cells (no diagonals allowed)".to_string() );
+		}
+		
+		let first;
+		let second;
+		
+		use std::cmp::Ordering::*;
+		
+		match cell1.y.cmp(&cell2.y)
+		{
+			Less =>
+			{
+				first = cell1;
+				second = cell2;
+			}
+			
+			Greater =>
+			{
+				first = cell2;
+				second = cell1;
+			}
+			
+			Equal => match cell1.x.cmp(&cell2.x)
+			{
+				Less =>
+				{
+					first = cell1;
+					second = cell2;
+				}
+				
+				Greater =>
+				{
+					first = cell2;
+					second = cell1;
+				}
+				
+				Equal => unreachable!()
+			}
+		}
+		
+		Ok( Self( first, second ) )
 	}
 }
